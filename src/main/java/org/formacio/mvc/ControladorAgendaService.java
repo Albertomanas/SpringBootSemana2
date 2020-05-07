c4ab4df5df42e0f1bd6a5f0036d248d913036873
Albertomanas <amanas@cifpfbmoll.eu>
2020-05-07 15:28:56 +0200

    (feat:AgendaServiceController, OperationException): Añadido captación de excepciones

--- src/main/java/org/formacio/mvc/ControladorAgendaService.java
+++ src/main/java/org/formacio/mvc/ControladorAgendaService.java
@@ -2,8 +2,10 @@ package org.formacio.mvc;
 
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,6 +37,20 @@ public class ControladorAgendaService {
 		 * @RequestParam sirve para extraer la id para el endpoint
 		 * Permite obtener el telefono de la id X.
 		 */
+	}
+	
+	/*
+	 * 3r caso test, contacte/id, corresponer id pasado como parametro {id}
+	 * Solo tiene que aceptar JSON (MediaType.APPLICATION_JSON)
+	 */
+	
+	@RequestMapping(path = "/concatcte/{id}")
+	@ResponseBody
+	public Persona obtenerContacto(@PathVariable String id){
+		if(agendaService == null) {
+			throw new OperationException();
+		} else
+			return agendaService.recupera(id);
 	}
 	
 }
